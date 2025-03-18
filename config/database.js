326d09e16f66804614364c0a770a5901f51cc1cb
ajfahim <ajfahim52@gmail.com>
2025-03-19 04:55:48 +0600

    somp

--- config/database.js
+++ config/database.js
@@ -26,10 +26,10 @@ const config = {
     logging: false,
   },
   production: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
+    username: process.env.DB_USER || "postgres",
+    password: process.env.DB_PASSWORD || "root",
+    database: process.env.DB_NAME || "call-bot",
+    host: process.env.DB_HOST || "localhost",
     dialect: "postgres",
     logging: false,
     pool: {
