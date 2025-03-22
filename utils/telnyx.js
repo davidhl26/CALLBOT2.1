3f6c680ade0d2f31187442ec67ae7f5d7d6d66a4
ajfahim <ajfahim52@gmail.com>
2025-03-22 12:25:03 +0600

    incomplete

---
+++ utils/telnyx.js
@@ -0,0 +1,33 @@
+import axios from "axios";
+
+export const hangupCall = async (callControlId) => {
+  console.log("[Telnyx] Hanging up call");
+  let data = JSON.stringify({
+    Texml: `
+    <Response>
+      <Hangup />
+    </Response>
+    `,
+  });
+
+  let config = {
+    method: "post",
+    maxBodyLength: Infinity,
+    url: `https://api.telnyx.com/v2/texml/calls/${callControlId}/update`,
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+      Authorization: `Bearer ${process.env.TELNYX_API_KEY}`,
+    },
+    data: data,
+  };
+
+  try {
+    const response = await axios.request(config);
+    console.log("[Telnyx] Hangup response:", response.data);
+    return response;
+  } catch (error) {
+    console.error("[Telnyx] Error hanging up call:", error);
+    return null;
+  }
+};
